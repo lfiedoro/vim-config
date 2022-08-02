18a2fcfaa6ee94b31008785eb6523fc87da7e564
Arkadiusz Hiler <arek@hiler.eu>
2022-08-03 01:29:32 +0300

    plugins: Configure Lua LSP for nvim plugin development

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -136,7 +136,13 @@ return require('packer').startup(function(use)
       lspconfig[lsp].setup{
         on_attach = on_attach,
         capabilities = capabilities,
-        settings = { Lua = { diagnostics = { globals = { 'vim' } } } }
+        settings = {
+          Lua = {
+            runtime = { version = "LuaJIT" },
+            diagnostics = { globals = { 'vim' } },
+            workspace = { library = vim.api.nvim_get_runtime_file("", true) }
+          }
+        }
       }
     end
   end }
