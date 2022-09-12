bb0e406bcc53cca8638b6096bf0a9d88b01b6662
Arkadiusz Hiler <arek@hiler.eu>
2022-09-12 23:02:27 +0300

    Rewrite presenter mode around orgmode treesitter and zen-mode

--- lua/user/present.lua
+++ lua/user/present.lua
@@ -1,34 +1,66 @@
-vim.w.presentation_mode = true
+local parser =  vim.treesitter.get_parser(0)
+local trees = parser:parse()
+local root = trees[1]:root()
+local slides = {}
 
-vim.wo.number = false
-vim.wo.relativenumber = false
+for i = 0, root:named_child_count() - 1 do
+  local node = root:named_child(i)
+  local text = vim.treesitter.query.get_node_text(node, 0)
+  table.insert(slides, text)
+end
 
-vim.go.laststatus = 0
-vim.go.ruler = false
-vim.go.showtabline = 0
+table.insert(slides, " THE END")
 
-vim.wo.cursorline = false
-vim.wo.signcolumn = 'no'
+local buf = vim.api.nvim_create_buf(true, true)
+vim.api.nvim_buf_set_name(buf, 'presentation')
+vim.api.nvim_win_set_buf(0, buf)
 
-vim.g.indent_blankline_enabled = false
+local function set_slide(nr)
+  vim.api.nvim_buf_set_option(buf, 'modifiable', true)
+  vim.api.nvim_buf_set_lines(buf, 0, -1, false, vim.split(slides[nr], '\n'))
+  vim.api.nvim_buf_set_option(buf, 'modifiable', false)
+  vim.api.nvim_buf_set_option(buf, 'filetype', 'org')
+end
 
-vim.cmd [[echo]]
+local current_slide = 1
+set_slide(current_slide)
 
-vim.keymap.set('n', '<Right>', ']a', { remap = true })
-vim.keymap.set('n', '<Left>',  '[a', { remap = true })
+-- style
 
-vim.g.catppuccin_flavour = "latte" -- latte, frappe, macchiato, mocha
-
-local colors = require("catppuccin.palettes").get_palette()
-require("catppuccin").setup {
-    dim_inactive = { enabled = true },
-    custom_highlights = {
-      MiniStatuslineModeInsert  = { bg = colors.sky, fg = colors.text },
-      MiniStatuslineModeVisual  = { bg = colors.pink },
-      MiniStatuslineModeReplace = { bg = colors.maroon },
-      MiniStatuslineModeCommand = { bg = colors.yellow },
-      MiniStatuslineModeOther   = { bg = colors.lavender },
-    },
-}
-
+vim.g.catppuccin_flavour = "latte"
 vim.cmd [[colorscheme catppuccin]]
+vim.g.indent_blankline_enabled = false
+require('zen-mode').open()
+
+-- restore style
+
+local function restore_style()
+    require('zen-mode').close()
+    require('mini.bufremove').delete(buf, true)
+
+    vim.g.indent_blankline_enabled = true
+    vim.cmd [[colorscheme solarized]]
+    vim.g.termguicolors = false
+end
+
+-- mappings
+
+vim.keymap.set('n', 'n', function()
+  if current_slide == #slides then
+    restore_style()
+    return
+  end
+
+  current_slide = current_slide + 1
+  set_slide(current_slide)
+end,
+{ buffer = buf }
+)
+
+vim.keymap.set('n', 'p', function()
+  if current_slide == 1 then return end
+  current_slide = current_slide - 1
+  set_slide(current_slide)
+end,
+{ buffer = buf }
+)
